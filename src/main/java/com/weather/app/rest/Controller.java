743c07b73562ca5618bc8d5590fc4f22d497f2f5
Raghul Krishnan <raghul.sathya@gmail.com>
2020-04-11 13:27:04 -0400

    With current time and one year back time

--- src/main/java/com/weather/app/rest/Controller.java
+++ src/main/java/com/weather/app/rest/Controller.java
@@ -1,10 +1,11 @@
 package com.weather.app.rest;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Objects;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -27,22 +28,33 @@ public class Controller {
 
   @RequestMapping
   public ArrayList<WeatherModel> getWeatherData() throws IOException {
-    long currentTime = System.currentTimeMillis();
-    String oneYearMilliS = "200";
-    long oneYear = Long.parseLong(oneYearMilliS);
+    long currentTime = System.currentTimeMillis()/1000;
+    System.out.println(currentTime);
+    String oneYearMilliS = "31556952000";
+    long oneYear = Long.parseLong(oneYearMilliS)/1000;
     long oneYearBackTime = currentTime - oneYear;
     String latitude = "42.3";
     String longitude = "-32.8";
-    ResponseEntity<String> response = restTemplate.getForEntity("https://api.darksky.net/forecast/0b67f8f549800f7bdeccc85500ba9324/"+latitude+","+longitude, String.class);
-//    WeatherModel currentWeatherData = restTemplate.getForObject("https://api.darksky.net/forecast/0b67f8f549800f7bdeccc85500ba9324/"+latitude+","+longitude, WeatherModel.class);
-//    WeatherModel oneYearBackWeatherData = restTemplate.getForObject("https://api.darksky.net/forecast/0b67f8f549800f7bdeccc85500ba9324/"+latitude+","+longitude, WeatherModel.class);
+    ResponseEntity<String> response = restTemplate.getForEntity("https://api.darksky.net/forecast/0b67f8f549800f7bdeccc85500ba9324/"+ latitude + "," + longitude + "," + currentTime, String.class);
+    ResponseEntity<String> responseOneYearBack = restTemplate.getForEntity("https://api.darksky.net/forecast/0b67f8f549800f7bdeccc85500ba9324/"+ latitude + "," + longitude + "," + oneYearBackTime, String.class);
     JsonNode responseNode = new ObjectMapper().readTree(Objects.requireNonNull(response.getBody()));
     WeatherModel currentWeatherData = new WeatherModel();
     currentWeatherData.setLongitude(longitude);
     currentWeatherData.setLatitude(latitude);
-    System.out.println(responseNode.get("currently").get("temperature").toString());
     currentWeatherData.setTemperature(responseNode.get("currently").get("temperature").toString());
-    currentWeatherData.setSunRiseTime(responseNode.get("daily").get("data").toString());
+    ArrayNode dailyData = (ArrayNode) responseNode.get("daily").get("data");
+    Iterator<JsonNode> dailyIterator = dailyData.elements();
+    while (dailyIterator.hasNext()) {
+      JsonNode dailyNode = dailyIterator.next();
+      currentWeatherData.setSunRiseTime(dailyNode.get("sunriseTime").toString());
+      currentWeatherData.setSunsetTime(dailyNode.get("sunsetTime").toString());
+      currentWeatherData.setTemperatureHigh(dailyNode.get("temperatureHigh").toString());
+      currentWeatherData.setTemperatureHighTime(dailyNode.get("temperatureHighTime").toString());
+      currentWeatherData.setTemperatureLow(dailyNode.get("temperatureLow").toString());
+      currentWeatherData.setTemperatureLowTime(dailyNode.get("temperatureLowTime").toString());
+    }
+
+//    currentWeatherData.setSunRiseTime(responseNode.get("dailyData").get("data").toString());
     ArrayList<WeatherModel> hey = new ArrayList<>();
     hey.add(currentWeatherData);
 //    hey.add(oneYearBackWeatherData);
