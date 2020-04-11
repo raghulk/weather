acd757ee8020769f832ae89467fc6f9a1fd674a0
Nirbhay Pherwani <pherwani37@gmail.com>
2020-04-11 11:40:02 -0400

    with array list

--- src/main/java/com/weather/app/rest/Controller.java
+++ src/main/java/com/weather/app/rest/Controller.java
@@ -1,5 +1,6 @@
 package com.weather.app.rest;
 
+import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,18 +20,18 @@ public class Controller {
   }
 
   @RequestMapping
-  public String getWeatherData() {
+  public ArrayList<WeatherModel> getWeatherData() {
     long currentTime = System.currentTimeMillis();
     String oneYearMilliS = "200";
     long oneYear = Long.parseLong(oneYearMilliS);
     long oneYearBackTime = currentTime - oneYear;
     String latitude = "42.3";
     String longitude = "-32.8";
-    WeatherModel currentWeatherData = restTemplate.getForObject("https://api.darksky.net/forecast/0b67f8f549800f7bdeccc85500ba9324/"+latitude+","+longitude+","+currentTime, WeatherModel.class);
-    WeatherModel oneYearBackWeatherData = restTemplate.getForObject("https://api.darksky.net/forecast/0b67f8f549800f7bdeccc85500ba9324/"+latitude+","+longitude+","+currentTime, WeatherModel.class);
-    StringBuilder response = new StringBuilder();
-    response.append(currentWeatherData);
-    response.append(oneYearBackWeatherData);
-    return response.toString();
+    WeatherModel currentWeatherData = restTemplate.getForObject("https://api.darksky.net/forecast/0b67f8f549800f7bdeccc85500ba9324/"+latitude+","+longitude, WeatherModel.class);
+    WeatherModel oneYearBackWeatherData = restTemplate.getForObject("https://api.darksky.net/forecast/0b67f8f549800f7bdeccc85500ba9324/"+latitude+","+longitude, WeatherModel.class);
+    ArrayList<WeatherModel> hey = new ArrayList<>();
+    hey.add(currentWeatherData);
+    hey.add(oneYearBackWeatherData);
+    return hey;
   }
 }
--- src/main/java/com/weather/app/rest/WeatherModel.java
+++ src/main/java/com/weather/app/rest/WeatherModel.java
@@ -1,6 +1,102 @@
 package com.weather.app.rest;
 
 public class WeatherModel {
+  public String getLatitude() {
+    return latitude;
+  }
+
+  public void setLatitude(String latitude) {
+    this.latitude = latitude;
+  }
+
+  public String getLongitude() {
+    return longitude;
+  }
+
+  public void setLongitude(String longitude) {
+    this.longitude = longitude;
+  }
+
+  public String getDate() {
+    return date;
+  }
+
+  public void setDate(String date) {
+    this.date = date;
+  }
+
+  public String getTime() {
+    return time;
+  }
+
+  public void setTime(String time) {
+    this.time = time;
+  }
+
+  public String getTemperature() {
+    return temperature;
+  }
+
+  public void setTemperature(String temperature) {
+    this.temperature = temperature;
+  }
+
+  public String getSunRiseTime() {
+    return sunRiseTime;
+  }
+
+  public void setSunRiseTime(String sunRiseTime) {
+    this.sunRiseTime = sunRiseTime;
+  }
+
+  public String getSunsetTime() {
+    return sunsetTime;
+  }
+
+  public void setSunsetTime(String sunsetTime) {
+    this.sunsetTime = sunsetTime;
+  }
+
+  public String getTemperatureHigh() {
+    return temperatureHigh;
+  }
+
+  public void setTemperatureHigh(String temperatureHigh) {
+    this.temperatureHigh = temperatureHigh;
+  }
+
+  public String getTemperatureHighTime() {
+    return temperatureHighTime;
+  }
+
+  public void setTemperatureHighTime(String temperatureHighTime) {
+    this.temperatureHighTime = temperatureHighTime;
+  }
+
+  public String getTemperatureLow() {
+    return temperatureLow;
+  }
+
+  public void setTemperatureLow(String temperatureLow) {
+    this.temperatureLow = temperatureLow;
+  }
+
+  public String getTemperatureLowTime() {
+    return temperatureLowTime;
+  }
+
+  public void setTemperatureLowTime(String temperatureLowTime) {
+    this.temperatureLowTime = temperatureLowTime;
+  }
+
+  public String getDaily() {
+    return daily;
+  }
+
+  public void setDaily(String daily) {
+    this.daily = daily;
+  }
+
   private String latitude;
   private String longitude;
   private String date;
@@ -12,4 +108,5 @@ public class WeatherModel {
   private String temperatureHighTime;
   private String temperatureLow;
   private String temperatureLowTime;
+  private String daily;
 }
